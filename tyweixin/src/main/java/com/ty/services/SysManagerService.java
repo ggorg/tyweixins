10df5944d91f99c20e10cb8202091eaee98e40fa
gen <84837891@qq.com>
2017-11-24 21:17:07 +0800

    增加用户列表

---
+++ tyweixin/src/main/java/com/ty/services/SysManagerService.java
@@ -0,0 +1,41 @@
+package com.ty.services;
+
+import com.gen.framework.common.beans.CommonCountBean;
+import com.gen.framework.common.beans.CommonSearchBean;
+import com.gen.framework.common.dao.CommonMapper;
+import com.gen.framework.common.util.Page;
+import org.apache.commons.beanutils.PropertyUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class SysManagerService {
+
+    @Autowired
+    private CommonMapper commonMapper;
+
+
+    public Page getUserList(Integer pageNum)throws Exception{
+
+        Page page=new Page(pageNum, 10);
+        Map<String,Object> condition=null;;
+
+
+        CommonSearchBean csb=new CommonSearchBean("baseUser","null",null, page.getStartRow(),page.getEndRow(),new HashMap<String,Object>());
+        CommonCountBean ccb = new CommonCountBean();
+
+        PropertyUtils.copyProperties(ccb, csb);
+        long count = commonMapper.selectCount(ccb);
+        if(count>0){
+            List list=this.commonMapper.selectObjects(csb);
+            page.setResult(list);
+            page.setTotal(count);
+        }
+
+        return page;
+    }
+}
