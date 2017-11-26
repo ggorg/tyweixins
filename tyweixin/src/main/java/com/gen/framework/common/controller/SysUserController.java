7acc91a79972d34e877dbf04ccaf832b5402ef29
deng_84837891@qq.com <Cbpwwksve>
2017-11-27 01:26:46 +0800

    ++

---
+++ tyweixin/src/main/java/com/gen/framework/common/controller/SysUserController.java
@@ -0,0 +1,33 @@
+package com.gen.framework.common.controller;
+
+import com.ty.services.SysManagerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/sys")
+public class SysUserController {
+    private final Logger logger = LoggerFactory.getLogger(SysUserController.class);
+    @Autowired
+    private SysManagerService sysManagerService;
+
+    @GetMapping("/user/list")
+    public String toUserList(@RequestParam(defaultValue = "1") Integer pageNo, ModelAndView model){
+        try {
+            model.addObject("userPage",this.sysManagerService.getUserList(pageNo));
+        }catch (Exception e){
+            logger.error("SysUserController->toUserList",e);
+        }
+        return "pages/manager/system/user";
+    }
+    @GetMapping("/user/edit")
+    public String toUserEdit(){
+        return "pages/manager/system/userEdit";
+    }
+}
---
+++ tyweixin/src/main/resources/static/sysframe2/js/custom/tables.js
@@ -0,0 +1,27 @@
+/*
+ * 	Additional function for tables.html
+ *	Written by ThemePixels	
+ *	http://themepixels.com/
+ *
+ *	Copyright (c) 2012 ThemePixels (http://themepixels.com)
+ *	
+ *	Built for Amanda Premium Responsive Admin Template
+ *  http://themeforest.net/category/site-templates/admin-templates
+ */
+
+jQuery(document).ready(function(){
+
+    jQuery('#dyntable2').dataTable({
+        "sPaginationType": "full_numbers",
+        "aaSortingFixed": [[0,'asc']],
+        "fnDrawCallback": function(oSettings) {
+            jQuery('input:checkbox,input:radio').uniform();
+            //jQuery.uniform.update();
+        }
+    });
+
+
+
+    ///// TRANSFORM CHECKBOX AND RADIO BOX USING UNIFORM PLUGIN /////
+    jQuery('input:checkbox,input:radio').uniform();
+});
